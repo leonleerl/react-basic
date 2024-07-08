7c6e15bbf1a915be683322d2cd562f387946b5b0
leonleerl <leon.runlongli@gmail.com>
2024-07-08 21:37:36 +0800

    remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 项目的入口
+import React from "react";
+import ReactDOM from "react-dom/client";
+// 导入项目的根组件
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 把App根组件渲染到id为root的dom节点上
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
