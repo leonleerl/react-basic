b20f218743ac306a9c2296bcdbda7cd2321e5c44
leonleerl <leon.runlongli@gmail.com>
2024-07-09 19:08:57 +0800

    add dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-// 项目的入口
-import React from "react";
-import ReactDOM from "react-dom/client";
-// 导入项目的根组件
-import App from "./App";
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-// 把App根组件渲染到id为root的dom节点上
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = createRoot(document.querySelector('#root'))
+
+root.render(<App />)
